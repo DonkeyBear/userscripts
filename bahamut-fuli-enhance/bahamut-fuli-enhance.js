bb2a314b77f7f09b4269989a1a29442f06d2c201
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2023-02-04 02:32:14 +0800

    Update bahamut-fuli-enhance.js

--- bahamut-fuli-enhance/bahamut-fuli-enhance.js
+++ bahamut-fuli-enhance/bahamut-fuli-enhance.js
@@ -1,13 +1,23 @@
 // ==UserScript==
 // @name         巴哈姆特勇者福利社++
 // @namespace    https://github.com/DonkeyBear
-// @version      0.3
+// @version      0.4
 // @description  改進巴哈姆特的勇者福利社，一次顯示全部商品，並加入過濾隱藏功能。
 // @author       DonkeyBear
 // @match        https://fuli.gamer.com.tw/shop.php*
-// @icon         https://i2.bahamut.com.tw/anime/baha_s.png
+// @icon         https://fuli.gamer.com.tw/favicon.ico
 // @grant        none
 // ==/UserScript==
+
+// 修正上排功能鈕的邊距，保持頁面樣式
+document.querySelector(".tabs-btn-box").style.marginTop = "0";
+
+// 依照 URL Param 判斷是否執行後續程式
+let newUrl = new URL(window.location);
+if (newUrl.searchParams.get("history") != "0" && newUrl.searchParams.get("history") != null) { return }
+if (newUrl.searchParams.get("page") != "1" && newUrl.searchParams.get("page") != null) {
+  window.location.replace("https://fuli.gamer.com.tw/shop.php");
+}
 
 function setupButton(labelText, keywordToHide, checkboxId) {
   let newFunctionButton = document.createElement("a");
@@ -34,45 +44,57 @@ function toggleItemCards(keywaord, hide) {
   }
 
   for (let card of cards) {
-    if (card.querySelector(".card-btn").innerText.includes(keywaord)) {
+    if (card.querySelector(".type-tag").innerText.includes(keywaord)) {
       card.style.display = styleDisplayString;
     }
   }
 }
 
-/* 更改 Header 上的名稱 */
-// 在不影響到 hover-bar 的情況下很難做得好看，因此先作罷。
-/* let fuliTitle = document.querySelector(".dropList a");
-fuliTitle.style.position = "relative";
-let newSpan = document.createElement("span");
-newSpan.style.position = "absolute";
-newSpan.style.right = ".15rem";
-newSpan.style.top = "-.3rem";
-newSpan.style.fontSize = ".8rem";
-newSpan.style.letterSpacing = "-.05em";
-newSpan.style.color = "rgb(234, 190, 122)";
-newSpan.innerText = "++";
-fuliTitle.appendChild(newSpan); */
+function countItemCards(keywaord) {
+  let counter = 0;
+  let cards = document.querySelectorAll("a.items-card");
+  for (let card of cards) {
+    if (card.querySelector(".type-tag").innerText.includes(keywaord)) {
+      counter++;
+    }
+  }
+  return counter;
+}
 
 /* 放置功能按鈕 */
-setupButton("隱藏兌換類", "兌換", "hide-exchange-items");
+setupButton("隱藏兌換類", "直購", "hide-exchange-items");
 setupButton("隱藏競標類", "競標", "hide-bid-items");
-setupButton("隱藏抽獎類", "抽獎", "hide-lottery-items");
-document.querySelector(".tabs-btn-box").style.marginTop = "0";
+setupButton("隱藏抽獎類", "抽抽樂", "hide-lottery-items");
+
+/* 放置商品類型計數區塊 */
+document.querySelector("#forum-lastBoard").insertAdjacentHTML("afterend", /* html */`
+  <div class="m-hidden">
+    <h5>現有商品數量</h5>
+    <div class="BH-rbox flex-center">
+      <span>兌換類：</span>
+      <span id="exchange-item-counter" style="color:#11AAC1;margin-right:.75rem"></span>
+      <span>競標類：</span>
+      <span id="bid-item-counter" style="color:#11AAC1;margin-right:.75rem"></span>
+      <span>抽獎類：</span>
+      <span id="lottery-item-counter" style="color:#11AAC1"></span>
+    </div>
+  </div>
+`);
+let exchangeItemCounter = document.getElementById("exchange-item-counter");
+let bidItemCounter = document.getElementById("bid-item-counter");
+let lotteryItemCounter = document.getElementById("lottery-item-counter");
+exchangeItemCounter.innerText = countItemCards("直購");
+bidItemCounter.innerText = countItemCards("競標");
+lotteryItemCounter.innerText = countItemCards("抽抽樂");
 
 /* 動態載入全部商品 */
-// 依照 URL Param 判斷是否執行
-let newUrl = new URL(window.location);
-if (newUrl.searchParams.get("history") != "0" && newUrl.searchParams.get("history") != null) { return }
-if (newUrl.searchParams.get("page") != "1" && newUrl.searchParams.get("page") != null) { return }
-
 document.getElementById("BH-pagebtn").style.display = "none"; // 隱藏選頁按鈕區塊
 let itemListBox = document.querySelector(".item-list-box");
 let maxPage = document.querySelector(".BH-pagebtnA a:last-child").innerText;
 if (maxPage == "1") { return } // 若僅一頁則不需讀取
 for (let page = 2; page <= maxPage; page++) {
   let xhr = new XMLHttpRequest();
-  let xhrUrl = `https://fuli.gamer.com.tw/shop.php?page=${page}&history=0`;
+  let xhrUrl = `https://fuli.gamer.com.tw/shop.php?page=${page}`;
   xhr.open("GET", xhrUrl, true);
   xhr.send();
   xhr.onreadystatechange = function () {
@@ -81,9 +103,12 @@ for (let page = 2; page <= maxPage; page++) {
       let virtualDoc = parser.parseFromString(this.responseText, 'text/html');
 
       let items = virtualDoc.querySelectorAll(".item-list-box a.items-card");
-      for (let item of items) {
-        itemListBox.appendChild(item);
-      }
+      for (let item of items) { itemListBox.appendChild(item); }
+
+      // 更新計數器
+      exchangeItemCounter.innerText = countItemCards("兌換");
+      bidItemCounter.innerText = countItemCards("競標");
+      lotteryItemCounter.innerText = countItemCards("抽獎");
     }
   }
 }
