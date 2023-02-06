91e599132e159f77502857ccb76f1f71d52c5dfd
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2023-02-07 03:54:33 +0800

    Update bahamut-fuli-enhance.js

--- bahamut-fuli-enhance/bahamut-fuli-enhance.js
+++ bahamut-fuli-enhance/bahamut-fuli-enhance.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         巴哈姆特勇者福利社++
 // @namespace    https://github.com/DonkeyBear
-// @version      0.5.0
-// @description  改進巴哈姆特的勇者福利社，一次顯示全部商品，並加入過濾隱藏功能。
+// @version      0.6.0
+// @description  改進巴哈姆特的勇者福利社，動態載入全部商品、加入過濾隱藏功能、標示競標目前出價等。
 // @author       DonkeyBear
 // @match        https://fuli.gamer.com.tw/shop.php*
 // @icon         https://fuli.gamer.com.tw/favicon.ico
@@ -17,6 +17,13 @@ let newUrl = new URL(window.location);
 if (newUrl.searchParams.get("history") != "0" && newUrl.searchParams.get("history") != null) { return }
 if (newUrl.searchParams.get("page") != "1" && newUrl.searchParams.get("page") != null) {
   window.location.replace("https://fuli.gamer.com.tw/shop.php");
+}
+
+// ".items-card .type-tag" 的內文
+const TYPE_TAG = {
+  exchange: "直購",
+  bid: "競標",
+  lottery: "抽抽樂"
 }
 
 function setupButton(labelText, keywordToHide, checkboxId) {
@@ -60,10 +67,33 @@ function countItemCards(keywaord) {
   return counter;
 }
 
+function getCurrentBid(itemsCardElement) {
+  if (itemsCardElement.querySelector('.type-tag').innerText != TYPE_TAG.bid) { return } // 若非競標品則結束函式
+  let xhr = new XMLHttpRequest();
+  let xhrUrl = itemsCardElement.href;
+  xhr.open("GET", xhrUrl);
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && (this.statusText == "OK" || this.status == 200)) {
+      let parser = new DOMParser();
+      let virtualDoc = parser.parseFromString(this.responseText, 'text/html');
+
+      let currentBid;
+      for (let item of virtualDoc.querySelectorAll('.pbox-content')) {
+        if (!item.innerText.includes("目前出價")) { continue }
+        currentBid = item.querySelector(".pbox-content-r").innerText.match(/[\d|,]+/)[0];
+        break;
+      }
+      let newTextHTML = /* html */`目前出價<p class="digital" style="margin-left:8px">${currentBid}</p>巴幣`;
+      itemsCardElement.querySelector(".price").innerHTML = newTextHTML;
+    }
+  }
+}
+
 /* 放置功能按鈕 */
-setupButton("隱藏兌換類", "直購", "hide-exchange-items");
-setupButton("隱藏競標類", "競標", "hide-bid-items");
-setupButton("隱藏抽獎類", "抽抽樂", "hide-lottery-items");
+setupButton("隱藏兌換類", TYPE_TAG.exchange, "hide-exchange-items");
+setupButton("隱藏競標類", TYPE_TAG.bid, "hide-bid-items");
+setupButton("隱藏抽獎類", TYPE_TAG.lottery, "hide-lottery-items");
 
 /* 放置商品類型計數區塊 */
 document.querySelector("#forum-lastBoard").insertAdjacentHTML("afterend", /* html */`
@@ -82,32 +112,50 @@ document.querySelector("#forum-lastBoard").insertAdjacentHTML("afterend", /* htm
 let exchangeItemCounter = document.getElementById("exchange-item-counter");
 let bidItemCounter = document.getElementById("bid-item-counter");
 let lotteryItemCounter = document.getElementById("lottery-item-counter");
-exchangeItemCounter.innerText = countItemCards("直購");
-bidItemCounter.innerText = countItemCards("競標");
-lotteryItemCounter.innerText = countItemCards("抽抽樂");
+exchangeItemCounter.innerText = countItemCards(TYPE_TAG.exchange);
+bidItemCounter.innerText = countItemCards(TYPE_TAG.bid);
+lotteryItemCounter.innerText = countItemCards(TYPE_TAG.lottery);
+
+/* 取得競標類商品的目前出價並標示於商品卡 */
+for (let card of document.querySelectorAll("a.items-card")) { getCurrentBid(card) }
 
 /* 動態載入全部商品 */
 document.getElementById("BH-pagebtn").style.display = "none"; // 隱藏選頁按鈕區塊
 let itemListBox = document.querySelector(".item-list-box");
 let maxPage = document.querySelector(".BH-pagebtnA a:last-child").innerText;
 if (maxPage == "1") { return } // 若僅一頁則不需讀取
+const observer = new MutationObserver((record) => {
+  // 建立觀測器，觀測新加入的商品卡
+  for (let newNode of record.addedNodes) {
+    if (!newNode.classList.contains("items-card")) { continue }
+    // 依照商品卡種類，增加計數和取得目前出價
+    switch (newNode.querySelector(".type-tag").innerText.trim()) {
+      case TYPE_TAG.exchange:
+        exchangeItemCounter.innerText++;
+        break;
+      case TYPE_TAG.bid:
+        bidItemCounter.innerText++;
+        getCurrentBid(newNode);
+        break;
+      case TYPE_TAG.lottery:
+        lotteryItemCounter.innerText++;
+        break;
+    }
+  }
+});
+observer.observe(document.querySelector(".item-list-box"), { childList: true });
 for (let page = 2; page <= maxPage; page++) {
   let xhr = new XMLHttpRequest();
   let xhrUrl = `https://fuli.gamer.com.tw/shop.php?page=${page}`;
-  xhr.open("GET", xhrUrl, true);
+  xhr.open("GET", xhrUrl);
   xhr.send();
   xhr.onreadystatechange = function () {
-    if (this.readyState === 4 && (this.statusText === "OK" || this.status === 200)) {
+    if (this.readyState == 4 && (this.statusText == "OK" || this.status == 200)) {
       let parser = new DOMParser();
       let virtualDoc = parser.parseFromString(this.responseText, 'text/html');
 
       let items = virtualDoc.querySelectorAll(".item-list-box a.items-card");
-      for (let item of items) { itemListBox.appendChild(item); }
-
-      // 更新計數器
-      exchangeItemCounter.innerText = countItemCards("直購");
-      bidItemCounter.innerText = countItemCards("競標");
-      lotteryItemCounter.innerText = countItemCards("抽抽樂");
+      for (let item of items) { itemListBox.appendChild(item) }
     }
   }
 }
