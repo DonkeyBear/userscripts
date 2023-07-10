20a22a39f2e2dec6ec462195489de194dfb818d1
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2023-07-10 21:31:05 +0800

    Create missav-autopause-blocker.js

---
+++ missav-autopause-blocker/missav-autopause-blocker.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         MissAV 防止自動暫停
+// @namespace    https://github.com/DonkeyBear
+// @version      0.1
+// @description  防止 MissAV 在切換視窗視窗、標籤時自動暫停
+// @author       DonkeyBear
+// @match        https://missav.com/*
+// @icon         https://missav.com/img/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+const videoPlayer = document.querySelector('.videoPlayer');
+if (!videoPlayer) { return }
+
+let windowIsBlurred = false;
+window.onblur = () => { windowIsBlurred = true };
+window.onfocus = () => { windowIsBlurred = false };
+
+videoPlayer.onpause = () => {
+  if (windowIsBlurred) { videoPlayer.play() }
+};
