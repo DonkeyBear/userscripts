3f5471d9d3b95fd0836fec55fd464ca594ff266a
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2025-01-13 14:39:46 +0800

    Update missav-autopause-blocker.js

--- missav-autopause-blocker/missav-autopause-blocker.js
+++ missav-autopause-blocker/missav-autopause-blocker.js
@@ -1,14 +1,24 @@
 // ==UserScript==
-// @name         MissAV 防止自動暫停
+// @name         MissAV 迷你加強包
 // @namespace    https://github.com/DonkeyBear
-// @version      0.2
-// @description  防止 MissAV 在切換視窗視窗、標籤時自動暫停
+// @version      0.3.0
+// @description  防止 MissAV 自動暫停、禁用彈出廣告，並將 missav.com 重新導向到 missav.ws
 // @author       DonkeyBear
 // @match        https://missav.com/*
-// @icon         https://missav.com/img/favicon.ico
+// @match        https://missav.ws/*
+// @icon         https://missav.ws/img/favicon.ico
 // @grant        none
 // ==/UserScript==
 
+window.open = () => { };
+
+// 將 missav.com 重新導向到 missav.ws
+const url = window.location.href;
+if (/^https:\/\/missav\.com/.test(url)) {
+  window.location.href = url.replace('missav.com', 'missav.ws');
+}
+
+// 防止自動暫停
 const videoPlayer = document.querySelector('video.player');
 if (!videoPlayer) { return }
 
